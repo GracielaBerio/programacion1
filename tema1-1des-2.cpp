4e2ab8cba6f27736e0e204cd79fd5e43f470214f
GracielaBerio <graciela.berio@docente.ceibal.edu.uy>
2025-04-04 01:52:26 +0000

    actualizacion des1

--- tema1-1des-2.cpp
+++ tema1-1des-2.cpp
@@ -6,11 +6,13 @@ using namespace std;
 int main(){
     int largo, ancho, area;
     string texto1="El valor del area es: ",texto2= " y los valores de ancho y largo son: ";
+    string explicacion="Este programa solicita el largo y ancho para calcular el área de un rectángulo y mostrarla" ;
     cout << "Ingrese el largo" <<endl;
     cin >> largo;
     cout << "Ingrese el ancho" << endl;
     cin >> ancho;
     area = largo * ancho;
+    cout << explicacion << endl;
     cout << texto1 << area << texto2 << largo << " y " <<ancho << endl;
     return 0;
 }
