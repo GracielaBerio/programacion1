db221cbacfe2dde38591d44de45a23f87397d54d
GracielaBerio <graciela.berio@docente.ceibal.edu.uy>
2025-04-04 01:33:37 +0000

    primer desafio

---
+++ tema1-1des-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int num1, num2, suma;
+    printf("Ingrese un numero\n");
+    scanf("%d",&num1);
+    printf("Ingrese otro numero\n");
+    scanf("%d",&num2);
+    suma=num1+num2;
+    printf("El resultado de la suma es %d\n",suma); 
+}
